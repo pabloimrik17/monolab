33524c46b8ca9a691868ff500aad7509061db807
Etherless <pabloimrik17@gmail.com>
2025-04-27 17:50:58 +0200

    chore: added eslint to lint staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,19 +2,16 @@
 const ESLINT_MAX_WARNINGS = 60;
 const KNIP_MAX_ISSUES = 100;
 
+// eslint-disable-next-line no-undef
 module.exports = {
     "**/*": [
         // the callback function is used to avoid files in the commit being
         // passed to knip
+        `eslint --max-warnings ${ESLINT_MAX_WARNINGS}`,
         () => `knip --max-issues ${KNIP_MAX_ISSUES}`,
         "prettier --write --ignore-unknown",
     ],
-    "*.{tsx,ts}": [`eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`],
-    // "*.{tsx,ts}": (fileNames) => [
-    //     `eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`,
-    //     // `prettier ${fileNames.join(" ")} --write --ignore-unknown`,
-    // ],
-    "*.md": "markdownlint --fix",
+    // "*.md": "markdownlint --fix",
     "*.css": "stylelint --fix",
     "*.scss": "stylelint --fix",
 };
