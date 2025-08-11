b5004020360b627622aeb0793958e22fabfacfea
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-08-11 22:14:55 +0200

    chore(@monolab/react-clean): init

---
+++ packages/react-clean/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * A function that returns a string "cool".
+ *
+ *
+ * @returns {string} Returns the string "cool".
+ */
+export const coolReactFunction = (): "cool" => {
+    return "cool";
+};
