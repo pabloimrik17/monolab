09509d04fe367f927feb20e08d3cca17b0a71778
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-08-11 22:37:02 +0200

    feat(@monolab/react-clean): added maybeCoolFunction

--- packages/react-clean/src/index.ts
+++ packages/react-clean/src/index.ts
@@ -7,3 +7,12 @@
 export const coolReactFunction = (): "cool" => {
     return "cool";
 };
+
+/**
+ * A function that randomly returns either "cool" or "notCool".
+ *
+ * @returns {("cool" | "notCool")} Returns either "cool" or "notCool" randomly.
+ */
+export const maybeCoolFunction = (): "cool" | "notCool" => {
+    return Math.random() > 0.5 ? "cool" : "notCool";
+};
