61abcdbcf267a2ae65fcfd5e720d13e35174e831
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-08-19 00:01:02 +0200

    feat(@monolab/react-clean): added lifecycles

--- packages/react-clean/src/use-view-model.hook.ts
+++ packages/react-clean/src/use-view-model.hook.ts
@@ -1,18 +1,32 @@
+import { useDidMount, useWillUnmount } from "@monolab/react-hooks";
 import { useState } from "react";
 import { BaseViewModel } from "./base.viewmodel.js";
 
 /**
- * Custom hook that initializes and returns a single instance of a ViewModel.
- * The ViewModel instance persists across component renders.
+ * A custom React hook for managing lifecycle events of a `BaseViewModel` instance.
  *
- * @template T The type extending the BaseViewModel.
- * @param {() => T} viewModel A factory function that creates the ViewModel instance.
- * @returns {T} The single instance of the provided ViewModel.
+ * This hook initializes a `BaseViewModel` instance, invokes its `didMount` lifecycle method
+ * during the component's mount phase, and ensures its `willUnmount` lifecycle method is
+ * called during the component's unmount phase. It simplifies the integration of
+ * `BaseViewModel` instances with React components.
+ *
+ * @template T Extends the `BaseViewModel` type.
+ * @param {() => T} viewModel A factory function that returns a new instance of a class
+ * that extends `BaseViewModel`.
+ * @returns {T} The initialized `BaseViewModel` instance.
  */
 export const useViewModel = <T extends BaseViewModel>(
     viewModel: () => T
 ): T => {
     const [instance] = useState<T>(() => viewModel());
+
+    useDidMount(async () => {
+        await instance.didMount();
+    });
+
+    useWillUnmount(() => {
+        instance.willUnmount();
+    });
 
     return instance;
 };
