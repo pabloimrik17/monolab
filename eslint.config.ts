7f5706f3434bfea1239609ded940d6abf550fd64
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-09-14 01:19:16 +0200

    Merge pull request feature/MON-19-knip (#31) from feature/MON-19-knip
    * chore: updated knip config
    
    * chore: added new commands
    
    * ci: improved CI
    
    * ci: remove accidental nx no-cloud flag
    
    * chore: updated nx config
    
    * ci: moved nx agent before install
    
    * chore: removed covera/html from workspace root
    
    * chore: added new monorepo clean command
    
    * chore: added ignore folder for eslint
    
    * chore: minor improvements

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,14 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    {
+        ignores: [
+            "**/dist/**",
+            "**/node_modules/**",
+            "**/coverage/**",
+            "**/html/**",
+        ],
+    },
     eslint.configs.recommended,
     tseslint.configs.recommended
 );
