3e92324a7bba802065ca5b021de0304d19c0dd32
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-11-09 11:13:46 +0100

    docs(is-even): add JSDoc examples to functions

--- packages/is-even/src/index.ts
+++ packages/is-even/src/index.ts
@@ -4,11 +4,26 @@ import { isOdd } from "@monolab/is-odd";
  * Checks if a number is even.
  * @param value - The number to check
  * @returns True if the number is even, false otherwise
+ * @example
+ * ```ts
+ * isEven(2) // true
+ * isEven(3) // false
+ * ```
  */
 export function isEven(value: number): boolean {
     return !isOdd(value);
 }
 
+/**
+ * Checks if a number is not even (i.e., odd).
+ * @param value - The number to check
+ * @returns True if the number is not even (odd), false otherwise
+ * @example
+ * ```ts
+ * isNotEven(3) // true
+ * isNotEven(2) // false
+ * ```
+ */
 export function isNotEven(value: number): boolean {
     return !isEven(value);
 }
