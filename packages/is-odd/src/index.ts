6ec39c03377261652f391617511c31cdfcee38b9
Pablo F. Guerra <pabloimrik17@gmail.com>
2025-11-09 11:18:47 +0100

    docs(is-odd): add JSDoc examples to isOdd function

--- packages/is-odd/src/index.ts
+++ packages/is-odd/src/index.ts
@@ -2,6 +2,11 @@
  * Checks if a number is odd.
  * @param value - The number to check
  * @returns True if the number is odd, false otherwise
+ * @example
+ * ```ts
+ * isOdd(3) // true
+ * isOdd(2) // false
+ * ```
  */
 export function isOdd(value: number): boolean {
     return value % 2 !== 0;
